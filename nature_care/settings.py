e0531638319709d3fb0880471906307d0f51f914
emiberndt <emi.berndt@gmail.com>
2021-07-29 21:28:48 +0000

    added checkout forms and templates

--- nature_care/settings.py
+++ nature_care/settings.py
@@ -45,7 +45,11 @@ INSTALLED_APPS = [
     'products',
     'bag',
     'checkout',
+
+    #Other
+    'crispy_forms',
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -58,6 +62,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'nature_care.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -74,8 +80,13 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
